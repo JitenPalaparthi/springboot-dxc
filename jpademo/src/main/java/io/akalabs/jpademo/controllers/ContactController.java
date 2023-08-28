01f180cbd90423b2c448d1fb281089c624df1b88
Jiten <jpalaparthi@gmail.com>
2023-08-28 12:12:40 +0530

    add get handlers

--- jpademo/src/main/java/io/akalabs/jpademo/controllers/ContactController.java
+++ jpademo/src/main/java/io/akalabs/jpademo/controllers/ContactController.java
@@ -3,7 +3,13 @@ package io.akalabs.jpademo.controllers;
 import io.akalabs.jpademo.models.Contact;
 import io.akalabs.jpademo.repositories.ContactRepository;
 
+import java.time.Instant;
+import java.util.Optional;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +32,28 @@ public class ContactController {
         //5- Response, which is in the form of Json
         //6- ensure json reponse headers and json response data
 
-        // contact.setStatus("active");
+         contact.setStatus("active");
+        // long tmeinMilli = Instant.now().toEpochMilli();
+         contact.setLastModified(Instant.now().toEpochMilli());
+         //Long lngtme= (Long)tmeinMilli;
         
       return contactRepository.save(contact);
          
+    }
+
+    @GetMapping("/contact/{id}")
+    public Optional<Contact> geContactById(@PathVariable("id") Long id){
+        return contactRepository.findById(id);
+    }
+
+    @GetMapping("/contact/byName/{name}")
+    public List<Contact> getContactsByName(@PathVariable("name") String name){
+        return contactRepository.findByName(name);
+       // return contactRepository.findAll(null, null);
+    }
+    @GetMapping("/contact/byEmail/{email}")
+    public List<Contact> getContactsByEmail(@PathVariable("email") String email){
+        return contactRepository.findByEmail(email);
+       // return contactRepository.findAll(null, null);
     }
 }
--- jpademo/src/main/java/io/akalabs/jpademo/repositories/ContactRepository.java
+++ jpademo/src/main/java/io/akalabs/jpademo/repositories/ContactRepository.java
@@ -2,7 +2,10 @@ package io.akalabs.jpademo.repositories;
 
 import io.akalabs.jpademo.models.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ContactRepository extends JpaRepository<Contact,Long> {
-    
+    List<Contact> findByName(String name);
+    List<Contact> findByEmail(String email);
 }
